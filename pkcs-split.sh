1bb0fd51682b17ce8e04f0eb32923918a9e62f07
Matt Ronchetto <hello@doamatto.xyz>
2022-04-13 22:25:12 -0700

    chore: remove encryption on key
    Needed for nginx afaik

--- pkcs-split.sh
+++ pkcs-split.sh
@@ -20,13 +20,13 @@ fi
 # Convert
 echo "Converting $1 into $2"
 if $2 == "PEM"
-  openssl pkcs12 -provider default -provider legacy -in $1 -nocerts -out $curwd/private.pem
+  openssl pkcs12 -provider default -provider legacy -in $1 -nocerts -nodes -out $curwd/private.pem
   openssl pkcs12 -provider default -provider legacy -in $1 -clcerts -nokeys -out $curwd/public.pem
 elif $2 == "KEY"
-  openssl pkcs12 -provider default -provider legacy -in $1 -nocerts -out $curwd/private.key
+  openssl pkcs12 -provider default -provider legacy -in $1 -nocerts -nodes -out $curwd/private.key
   openssl pkcs12 -provider default -provider legacy -in $1 -clcerts -nokeys -out $curwd/public.cer
 elif $2 == "RSA"
-  openssl pkcs12 -provider default -provider legacy -in $1 -nocerts -out $curwd/private.rsa
+  openssl pkcs12 -provider default -provider legacy -in $1 -nocerts -nodes -out $curwd/private.rsa
   openssl pkcs12 -provider default -provider legacy -in $1 -clcerts -nokeys -out $curwd/public.cer
 else
   echo "Unexpected error."
