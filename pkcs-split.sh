a57050678a8de2429965dab3000d2c43e797c30a
Matt Ronchetto <hello@doamatto.xyz>
2022-04-15 08:48:00 -0700

    chore: slew of changes...
    ... to create a (hopefully) functional tool

--- pkcs-split.sh
+++ pkcs-split.sh
@@ -3,34 +3,25 @@
 curwd=$(pwd)
 
 # Check if impromper conversion
-if $2 == "PEM"
-then
-  break
-elif $2 == "KEY"
-then
-  break
-elif $2 == "RSA"
-then
-  break
-else
-  echo "Unrecognised conversion format: $2"
-  return 1
-fi
+case $2 in
+        "PEM"|"KEY"|"RSA") echo "Valid input; continuing...";;
+        *) echo "Unrecognised conversion format: $2" && return 1;;
+esac
 
 # Convert
 echo "Converting $1 into $2"
-if $2 == "PEM"
-  openssl pkcs12 -provider default -provider legacy -in $1 -nocerts -nodes -out $curwd/private.pem
-  openssl pkcs12 -provider default -provider legacy -in $1 -clcerts -nokeys -out $curwd/public.pem
-elif $2 == "KEY"
-  openssl pkcs12 -provider default -provider legacy -in $1 -nocerts -nodes -out $curwd/private.key
-  openssl pkcs12 -provider default -provider legacy -in $1 -clcerts -nokeys -out $curwd/public.cer
-elif $2 == "RSA"
-  openssl pkcs12 -provider default -provider legacy -in $1 -nocerts -nodes -out $curwd/private.rsa
-  openssl pkcs12 -provider default -provider legacy -in $1 -clcerts -nokeys -out $curwd/public.cer
+if [ "$2" == "PEM" ]; then
+  openssl pkcs12 -provider default -provider legacy -in "$1" -nocerts -nodes -out "$curwd/private.pem"
+  openssl pkcs12 -provider default -provider legacy -in "$1" -clcerts -nokeys -out "$curwd/public.pem"
+elif [ "$2" == "KEY" ]; then
+  openssl pkcs12 -provider default -provider legacy -in "$1" -nocerts -nodes -out "$curwd/private.key"
+  openssl pkcs12 -provider default -provider legacy -in "$1" -clcerts -nokeys -out "$curwd/public.cer"
+elif [ "$2" == "RSA" ]; then
+  openssl pkcs12 -provider default -provider legacy -in "$1" -nocerts -nodes -out "$curwd/private.rsa"
+  openssl pkcs12 -provider default -provider legacy -in "$1" -clcerts -nokeys -out "$curwd/public.cer"
 else
   echo "Unexpected error."
   return 1
 fi
 
-echo "Conversion complete. See folder where certificates are for result.
+echo "Conversion complete. See folder where certificates are for result."
